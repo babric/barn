611aa4ebb4c1a66b1745f2193acf169975875e7d
js6pak <kubastaron@hotmail.com>
2022-03-19 19:08:34 +0100

    Initial mappings

---
+++ src/constants/java/net/fabricmc/yarn/constants/network/ClientCommandMode.java
@@ -0,0 +1,14 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.fabricmc.yarn.constants.network;
+
+public final class ClientCommandMode {
+    public static final int PRESS_SHIFT_KEY = 1;
+    public static final int RELEASE_SHIFT_KEY = 2;
+    public static final int STOP_SLEEPING = 3;
+
+    private ClientCommandMode() {
+    }
+}
